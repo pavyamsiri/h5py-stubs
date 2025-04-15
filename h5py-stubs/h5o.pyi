a1957a6fdea7dfc666ea363e508129bcccf3507c
Pavadol Yamsiri <pavyamsiri@gmail.com>
2025-04-15 16:28:02 +1000

    _objects: Update _objects to include all non-private attributes
    Non-private attributes are the attributes found when calling dir()
    on the module whose name does not start with _

---
+++ h5py-stubs/h5o.pyi
@@ -0,0 +1,14 @@
+from typing import Final
+
+from ._objects import phil
+
+TYPE_GROUP: Final[int]
+TYPE_DATASET: Final[int]
+TYPE_NAMED_DATATYPE: Final[int]
+
+COPY_SHALLOW_HIERARCHY_FLAG: Final[int]
+COPY_EXPAND_SOFT_LINK_FLAG: Final[int]
+COPY_EXPAND_EXT_LINK_FLAG: Final[int]
+COPY_EXPAND_REFERENCE_FLAG: Final[int]
+COPY_WITHOUT_ATTR_FLAG: Final[int]
+COPY_PRESERVE_NULL_FLAG: Final[int]
