4cf6df0295d73695c29f6c27551fe9bf2e760e37
Pavadol Yamsiri <pavyamsiri@gmail.com>
2025-04-15 20:55:07 +1000

    h5r: Add type stubs

--- h5py-stubs/h5r.pyi
+++ h5py-stubs/h5r.pyi
@@ -1,8 +1,36 @@
-from h5py._objects import ObjectID
-from h5py.h5s import SpaceID
+from typing import Final
 
-class Reference: ...
-class RegionReference(Reference): ...
+from ._objects import ObjectID, phil, with_phil
+from .h5p import PropID
+from .h5s import SpaceID
+
+__all__ = [
+    "DATASET_REGION",
+    "OBJECT",
+    "Reference",
+    "RegionReference",
+    "create",
+    "dereference",
+    "get_name",
+    "get_obj_type",
+    "get_region",
+    "phil",
+    "with_phil",
+]
+
+OBJECT: Final[int]
+DATASET_REGION: Final[int]
 
 def create(loc: ObjectID, name: str, ref_type: int, space: SpaceID | None = None) -> Reference: ...
 def get_region(ref: Reference, id: ObjectID) -> SpaceID | None: ...
+def get_name(ref: Reference, loc: ObjectID) -> bytes: ...
+def get_obj_type(ref: Reference, id: ObjectID) -> int | None: ...
+def dereference(ref: Reference, id: ObjectID, oapl: PropID | None = None) -> ObjectID | None: ...
+
+class Reference:
+    @property
+    def typecode(self) -> int: ...
+    @property
+    def typesize(self) -> int: ...
+
+class RegionReference(Reference): ...
