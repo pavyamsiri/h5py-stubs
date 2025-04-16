af68fe10953766c466d5594579907f27ae279edd
Pavadol Yamsiri <78515468+pavyamsiri@users.noreply.github.com>
2025-04-16 16:33:43 +1000

    Some work on the high level interface modules (#7)
    * _selector: Add type stubs
    
    * base: Adjust type stubs
    
    * attrs: Adjust type stubs
    
    * datatype: Add type stubs

--- h5py-stubs/_hl/attrs.pyi
+++ h5py-stubs/_hl/attrs.pyi
@@ -1,25 +1,49 @@
-from collections.abc import Iterator
+import uuid
 from typing import TypeAlias
 
-import numpy as np
+import numpy  # noqa: ICN001
+from h5py import h5, h5a, h5p, h5s, h5t
+from h5py._objects import phil, with_phil
 from h5py.h5a import AttrID
 from optype import numpy as onpt
 
-from .base import CommonStateObject, Empty, HLObject, MutableMappingHDF5
+from . import base
+from .base import CommonStateObject, Empty, HLObject, MutableMappingHDF5, is_empty_dataspace, product
+from .datatype import Datatype
+
+__all__ = [
+    "AttributeManager",
+    "Datatype",
+    "Empty",
+    "base",
+    "h5",
+    "h5a",
+    "h5p",
+    "h5s",
+    "h5t",
+    "is_empty_dataspace",
+    "numpy",
+    "phil",
+    "product",
+    "uuid",
+    "with_phil",
+]
 
 _AnyShape: TypeAlias = tuple[int, ...]
-_AnyArray: TypeAlias = np.ndarray[_AnyShape, onpt.DType]
+_AnyArray: TypeAlias = numpy.ndarray[_AnyShape, onpt.DType]
+_Attribute: TypeAlias = Empty | _AnyArray | numpy.generic
 
-class AttributeManager(MutableMappingHDF5, CommonStateObject):
+class AttributeManager(MutableMappingHDF5[str | bytes, _Attribute], CommonStateObject):
     def __init__(self, parent: HLObject) -> None: ...
-    def __getitem__(self, name: str | bytes) -> np.generic | _AnyArray | Empty: ...
+    def __getitem__(self, name: str | bytes) -> _Attribute: ...
     def get_id(self, name: str | bytes) -> AttrID: ...
     def __setitem__(self, name: str | bytes, value: object) -> None: ...
     def __delitem__(self, name: str | bytes) -> None: ...
     def create(
-        self, name: str | bytes, data: object, shape: tuple[int, ...] | None = None, dtype: onpt.AnyDType | None = None
+        self,
+        name: str | bytes,
+        data: object,
+        shape: tuple[int, ...] | int | None = None,
+        dtype: Datatype | onpt.AnyDType | None = None,
     ) -> None: ...
     def modify(self, name: str | bytes, value: object) -> None: ...
-    def __len__(self) -> int: ...
-    def __iter__(self) -> Iterator[str]: ...
-    def __contains__(self, name: object) -> bool: ...
--- h5py-stubs/_hl/compat.pyi
+++ h5py-stubs/_hl/compat.pyi
@@ -1,6 +1,20 @@
+import sys
+from os import fsdecode, fsencode, fspath
 from typing import Final
 
 from _typeshed import StrOrBytesPath
+from h5py.version import hdf5_built_version_tuple
+
+__all__ = [
+    "WINDOWS_ENCODING",
+    "filename_decode",
+    "filename_encode",
+    "fsdecode",
+    "fsencode",
+    "fspath",
+    "hdf5_built_version_tuple",
+    "sys",
+]
 
 WINDOWS_ENCODING: Final[str]
 
